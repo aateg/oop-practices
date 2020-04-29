f08fb2f7e1005da814603e7463f5db124074dea4
Guilherme Fernandes <aateg@live.com>
2020-04-29 11:13:18 -0300

    aula7

---
+++ 5.inheritance2/exercises/ex2aula7/RedeSocial.h
@@ -0,0 +1,24 @@
+#ifndef REDESOCIAL_H
+#define REDESOCIAL_H
+
+#include "Perfil.h"
+
+#include <string>
+using namespace std;
+
+class RedeSocial {
+    public:
+        RedeSocial (); // construtor
+        ~RedeSocial(); // destrutor
+        
+        Perfil **getPerfis();
+        int getQuantidadeDePerfis();
+        bool adicionar (Perfil *perfil);
+        void imprimir ();
+    private:
+        Perfil **perfis;
+        const int capacidade = 100;
+        int quantidadeDePerfis;
+};
+
+#endif //REDESOCIAL_H
